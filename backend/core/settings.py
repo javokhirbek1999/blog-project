d40455f9d4c3927976e42801f7d08d729854cc3e
Javokhirbek Khaydaraliev <khaydaraliev99@gmail.com>
2024-12-11 04:23:16 +0500

    Added custom user management

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third-party
+    'rest_framework',
+
+    'api',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +126,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'api.User'
